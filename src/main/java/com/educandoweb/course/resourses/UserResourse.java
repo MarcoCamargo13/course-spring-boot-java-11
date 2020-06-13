6d534a27501e4aea76dce62e2ddf04bceaed5ca3
Marco Camargo <marcopcamargo@gmail.com>
2020-06-13 09:19:32 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resourses/UserResourse.java
+++ src/main/java/com/educandoweb/course/resourses/UserResourse.java
@@ -1,28 +1,38 @@
 package com.educandoweb.course.resourses;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController // recurso web implementado por um controlador Rest// @ quer dizer anotation
 @RequestMapping(value = "/users") // dar um nome ao recurso, usar o mesmo da classe
 public class UserResourse {// disponibiliza recursos web correspondente a entidade User
-	// para testar o recurso Rest foi criado um end point para acessar os usuario
-	// para usar o Long da variavel na classe User o java solicita colocar o "id +
-	// L"
-	// REsponseEntity é um tipo especifico para retorno de resposta do Spring Boot
-	// <T> é um tipo generics, este tipo é da classe User
-	@GetMapping //para indicar que é um metodo que responde o Rest do HTTP é inserido um Anotation	
-	public ResponseEntity<User> findAll() {
-		User teste = new User(1L, "Marco", "m@m.com", "123456798", "123465798");//Cria um objeto atraves do contrutor
-		return ResponseEntity.ok(teste);
-		//utiliza o REsponseEntity para enviar a resposta do Rest
-		//ok para indicar a resposta OK
-		//body indica o corpo do retorno
+	@Autowired // para o Spring fazer a injeção de dependecia automatico
+	private UserService service; // dependencia do serviçe
 
+	 // para indicar que é um metodo que responde o Rest do HTTP é inserido um
+	@GetMapping			// Anotation
+	public ResponseEntity<List<User>> findAll() {
+		// o metodo findAll recebe uma lista de User
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 
+	//CRIANDO UM END POINT PARA BUSCAR UM SUSUARIO POR ID
+	@GetMapping(value = "/{id}")//vai indicar que o get recebe um id na URL
+	//@PathVariable para o Spring aceitar que passar como parametro(value = "/{id}") do tipo Long
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);//metodo para retorna um usuario pelo URL
+		
+	}
+	
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service // para indicar o registro no compnente do Spring para funcionar, temos tb Ex.
+			// @Component /@REpository
+public class UserService {// criado uma classe de serviço para cuidar das transações
+	@Autowired // para fazer uma injeção de dependencia de forma clara
+	private UserRepository repository;// injeção de dependecia
+
+	// metodo para retornar todos os usuarios de uma ista do tipo User
+	public List<User> findAll() {// criado um metodo que repassa a chamada para a camada findAll do
+									// UserRepository
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);//Optional é um objeto tipo <User>
+		return obj.get();
+	}
+}
