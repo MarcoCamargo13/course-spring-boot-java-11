4818d53fb35f6d6bfd03efe1f1e830064d37e0f9
Marco Camargo <marcopcamargo@gmail.com>
2020-06-15 10:28:10 -0300

    Order, Instant, ISO 8601 - part 1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,27 +1,37 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 //anotações indicando para JPA de como ira converter os objetos para o banco relacional
 //sempre depender da expecificação nunda da implementação (o maven cria os import dele)
 //para testar a configuração do H2 pom + application.properties , ir no navegador http://localhost:8080/h2-console
-@Entity
+@Entity //classe é uma entidade
+@Table(name = "tb_user")//colocando um anome a atabela
 public class User implements Serializable {// gera cadeia em bytes para trafegar em redes e criar arquivos
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //ANOTATION PARA INDICAR para JPA que o ID é autoincrementavel, funciona para SQl MYsql e outros
-	private Long id;//para o JPA ela é uma chave numerica e autoincrementavel
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // ANOTATION PARA INDICAR para JPA que o ID é autoincrementavel,
+	private Long id;// para o JPA ela é uma chave numerica e autoincrementavel
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
+
+	//(mappedBy = "client") --> para acessar os pedido opcional, indica que foi mapeado a tabela de cliente
+	@OneToMany(mappedBy = "client") //a anotação é para indicar para o banco que é muito para 1(varios pedidos para um cliente)
+	private List<Order> orders = new ArrayList<>();// foi criada uma associação de 1 ..muitos, foi criado um get
+													// somente(pq com o set trocariamos a lista)
 
 	public User() {// construtor vazio obrigatorio em Spring Boot
 
@@ -74,6 +84,10 @@ public class User implements Serializable {// gera cadeia em bytes para trafegar
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
